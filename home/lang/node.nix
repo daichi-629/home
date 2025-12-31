4ab61dc69681fb05b2d5573de83e140f64b45ea4
daichi-629 <57441686+daichi-629@users.noreply.github.com>
2025-12-31 23:41:58 +0900

    node setting

---
+++ home/lang/node.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.my.lang.node;
+in
+{
+  options.my.lang.node.enable = lib.mkEnableOption "Node.js toolchain";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      nodejs
+      pnpm
+    ];
+
+    programs.zsh.initExtra = ''
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+alias npm="pnpm"
+alias orgnpm="npm"
+    '';
+
+  };
+}
+
--- home/zsh/rc.zsh
+++ home/zsh/rc.zsh
@@ -28,9 +28,6 @@ source ~/powerlevel10k/powerlevel10k.zsh-theme
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 
@@ -39,21 +36,12 @@ export TMUX_TMPDIR=/tmp/
 setopt hist_ignore_dups
 setopt EXTENDED_HISTORY
 
-# pnpm
-export PNPM_HOME="$HOME/.local/share/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-# pnpm end
 
 
 
 export PATH=$PATH:$HOME/.local/bin
 
 alias ls="eza --icons=always --classify=always --hyperlink"
-alias npm="pnpm"
-alias orgnpm="npm"
 
 export PATH="$PATH:/usr/local/texlive/2025/bin/x86_64-linux"
 # Set up fzf key bindings and fuzzy completion
