881898f76b79b027e93d977ecc3546fce3f53b4b
daichi-629 <57441686+daichi-629@users.noreply.github.com>
2026-01-04 17:49:13 +0900

    add nixfmt

--- home/common.nix
+++ home/common.nix
@@ -1,16 +1,15 @@
-{config, pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 let
   mkRepo = import ./lib/mk-worktree-repo.nix { inherit lib pkgs; };
-  pinFile=../pins/repos.json;
+  pinFile = ../pins/repos.json;
   repo = mkRepo {
     pinKey = "my-nvim-config";
     workdirName = "my-nvim-config";
     pinsFile = pinFile;
     homeDir = config.home.homeDirectory;
   };
-in
-{
-  imports= [
+in {
+  imports = [
     ./lang/node.nix
     ./lang/tex.nix
     ./lang/rust.nix
@@ -19,7 +18,7 @@ in
     ./tools/codex.nix
   ];
   home.stateVersion = "25.11";
-  programs.home-manager.enable= true;
+  programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
     git
@@ -44,31 +43,29 @@ in
     wget
     zip
     unzip
+    nixfmt-rfc-style
   ];
   home.sessionVariables = {
-    EDITOR="nvim";
-    HISTSIZE=1000;
-    SAVEHIST=100000;
-    HISTFILE="$HOME/.zsh_history";
+    EDITOR = "nvim";
+    HISTSIZE = 1000;
+    SAVEHIST = 100000;
+    HISTFILE = "$HOME/.zsh_history";
   };
 
-  home.file."powerlevel10k".source =  "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
+  home.file."powerlevel10k".source =
+    "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
   home.file.".p10k.zsh".source = ../dotfiles/.p10k.zsh;
 
-  programs.git = {
-    enable = true;
-  };
-  programs.zsh = {
-    enable = true;
-  };
+  programs.git = { enable = true; };
+  programs.zsh = { enable = true; };
   programs.zsh.initContent = lib.mkMerge [
     (lib.mkOrder 1000 (builtins.readFile ./zsh/rc.zsh))
     (lib.mkOrder 1001 (builtins.readFile ./zsh/fzf.zsh))
   ];
-  home.activation=repo.activation;
+  home.activation = repo.activation;
 
- xdg.enable = true;  
- xdg.configFile."nvim".source=
-      config.lib.file.mkOutOfStoreSymlink repo.workdir;
-  xdg.configFile."zellij/config.kdl".source=../dotfiles/zellij/config.kdl;
+  xdg.enable = true;
+  xdg.configFile."nvim".source =
+    config.lib.file.mkOutOfStoreSymlink repo.workdir;
+  xdg.configFile."zellij/config.kdl".source = ../dotfiles/zellij/config.kdl;
 }
