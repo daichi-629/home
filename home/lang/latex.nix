d521e526fbce6ca6eb826d3239569491c5408160
daichi-629 <57441686+daichi-629@users.noreply.github.com>
2026-01-21 12:31:56 +0900

    fix

--- home/lang/latex.nix
+++ home/lang/latex.nix
@@ -6,7 +6,7 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [ texliveFull ];
     home.file.".latexindent.yaml".source = ../../dotfiles/.latexindent.yaml;
-    home.file.".indentconfig.yamlh".source = ../../dotfiles/.indentconfig.yaml;
+    home.file.".indentconfig.yaml".source = ../../dotfiles/.indentconfig.yaml;
   };
 }
 
