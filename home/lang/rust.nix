0867b7079af2a51c4ece1d4b8a266d4acf55a548
daichi-629 <57441686+daichi-629@users.noreply.github.com>
2026-02-20 14:46:29 +0900

    change rust installation

--- home/lang/rust.nix
+++ home/lang/rust.nix
@@ -7,10 +7,14 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages = [
       pkgs.gdb
-      (pkgs.rust-bin.stable.latest.default.override {
-        extensions = [ "rust-analyzer" "rust-src" "clippy" ];
-      })
+      pkgs.curl
     ];
+
+    home.activation.rustupInstall = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ ! -x "$HOME/.cargo/bin/rustup" ]; then
+        ${pkgs.curl}/bin/curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+      fi
+    '';
 
     # Make cargo-installed binaries (e.g., cargo-binstall) available on PATH.
     home.sessionPath = [ "$HOME/.cargo/bin" ];
