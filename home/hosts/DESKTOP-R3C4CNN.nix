a890150bcdc863173ea23fe6fdcba1ab4dde9cd5
daichi-629 <57441686+daichi-629@users.noreply.github.com>
2026-04-10 16:39:14 +0900

    add himalaya in unstable

--- home/hosts/DESKTOP-R3C4CNN.nix
+++ home/hosts/DESKTOP-R3C4CNN.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  pkgs_unstable,
+  config,
+  ...
+}:
 {
   my.lang.node.enable = true;
   my.lang.rust.enable = true;
@@ -10,16 +15,20 @@
   my.tools.playwright.enable = true;
   my.tools.copilot.enable = true;
 
-  home.packages = with pkgs; [
-    wl-clipboard
-    ni
-    graphviz-nox
-    flatpak
-    hledger
-    hledger-ui
-   neomutt
-   hledger-web
-  ];
+  home.packages =
+    with pkgs;
+    [
+      wl-clipboard
+      ni
+      graphviz-nox
+      flatpak
+      hledger
+      hledger-ui
+      hledger-web
+    ]
+    ++ (with pkgs_unstable; [
+      himalaya
+    ]);
   imports = [ ./DESKTOP-R3C4CNN/sops.nix ];
 
   home.sessionVariables = {
