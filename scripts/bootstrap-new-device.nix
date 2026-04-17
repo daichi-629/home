e7773a6d4268a36d946fe289e4d3e6ab09b6de97
daichi-629 <57441686+daichi-629@users.noreply.github.com>
2026-04-17 19:40:49 +0900

    Make nix-run bootstrap self-contained

--- scripts/bootstrap-new-device.nix
+++ scripts/bootstrap-new-device.nix
@@ -1,6 +1,15 @@
 { pkgs, self, ... }:
 {
   bootstrapNewDevice = pkgs.writeShellScriptBin "bootstrap-new-device" ''
+    export PATH="${pkgs.lib.makeBinPath [
+      pkgs.bash
+      pkgs.coreutils
+      pkgs.git
+      pkgs.hostname
+      pkgs.nix
+      pkgs.openssh
+      pkgs.python3
+    ]}:$PATH"
     exec ${pkgs.bash}/bin/bash ${self}/scripts/bootstrap-new-device.sh "$@"
   '';
 }
